0ba6003d334e2c524014ee60bc77f295fbbc0922
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-16 23:48:19 +0530

    first commit setup for react and tailwindcss done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <h1 className="text-6xl font-mono w-full h-screen flex justify-center items-center bg-black text-white">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
