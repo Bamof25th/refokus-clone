6d7df423e3a06eb455dfde52bb3c1ef8b378cc4b
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-18 23:20:32 +0530

    done products components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import Work from "./components/Work";
 import Stripes from "./components/Stripes";
+import Products from "./components/Products";
 
 function App() {
   return (
-    <div className=" w-full h-screen font-[satoshi_variable] bg-zinc-900 text-white">
+    <div className=" w-full h-full font-[satoshi_variable] bg-zinc-900 text-white">
       <Navbar />
       <Work />
       <Stripes />
+      <Products />
     </div>
   );
 }
